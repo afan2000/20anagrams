82f6742cd40c3fdef2bed5b03a6b40484b4c91ba
afan2000 <apronfag@gmail.com>
2017-01-10 10:14:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 int main()
 {
-	int wordTable[20][26] = { 0 };
-	int rowSize = sizeof(wordTable) / sizeof(wordTable[1]);
+	int wordTable[27][20] = { 0 };
+	int rowSize = (sizeof(wordTable) / sizeof(wordTable[1])) - 1;
 	int colSize = sizeof(wordTable) / rowSize;
 	cout << rowSize << '\n';
 	int i = 0;
+	int z = 1;
 	char c;
 
 
@@ -20,16 +21,26 @@ int main()
 		cout << "enter word " << i + 1 << ": ";
 		c = getchar();
 
-		for (; c != '\n';)
+		for (; c != '\n';)//puts letters of word into table array
 		{
 			c = toupper(c);
-			wordTable[i][c - 'A'] += 1;
+			wordTable[i][c - 'A'] += 1; //converts letters into decimal equivalent, adds 1 to position in array
 			c = getchar();
 		}
-		for (int j = 0; j < rowSize; j++)
+		for (int j = 0; j < rowSize; j++) //check for repeated digits in rows
 		{
-			if(a[i][j] == )
+			for (int x = i; x > 0; x--) //checks for repeated digits in columns
+			{
+				if (wordTable[j][x] != wordTable[j][x - 1])
+				{
+					wordTable[27][x] = z;
+					z++;
+					goto exitLoop;
+				}
+
+			}
 		}
+	exitLoop:
 	}
 }
 
