9ff427d1fd87d6e56364c1250ed9aebbeacac12a
afan2000 <apronfag@gmail.com>
2017-01-10 10:00:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
+// 20Anagrams.cpp : Defines the entry point for the console application.
+//
+
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
-  int letterTable[20][26] = {0};
-  char c = getchar();
-  
-  for
+	int wordTable[20][26] = { 0 };
+	int rowSize = sizeof(wordTable) / sizeof(wordTable[1]);
+	int colSize = sizeof(wordTable) / rowSize;
+	cout << rowSize << '\n';
+	int i = 0;
+	char c;
+
+
+	for (i; i < rowSize; i++)
+	{
+		cout << "enter word " << i + 1 << ": ";
+		c = getchar();
+
+		for (; c != '\n';)
+		{
+			c = toupper(c);
+			wordTable[i][c - 'A'] += 1;
+			c = getchar();
+		}
+		for (int j = 0; j < rowSize; j++)
+		{
+			if(a[i][j] == )
+		}
+	}
 }
+
