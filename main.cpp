b0071b860511b4d9b26bc6e10c3776f05c6ca4be
afan2000 <apronfag@gmail.com>
2017-01-29 17:42:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,56 +1,65 @@
-// 20Anagrams.cpp : Defines the entry point for the console application.
-//
-
 #include <iostream>
-using namespace std;
-
+#include "stdafx.h"
 
 int main()
 {
-	int wordTable[10][27] = { 0 };
+	char wordTable[7][30] = { "abcde", "aba", "abccc", "cccba", "edcba", "cccba", "aba" };
+	int occTable[7][26] = { 0 }; //occurence table that converts wordTable to decimal
+	int groupTable[7] = { 0 };
+	int rowSize = (sizeof(wordTable) / sizeof(wordTable[1])); ////how many rows
+	const int alphaLength = 26; //amount of letters in alphabet
+	char c; //used to get characters
+	int j = 0; //marks position of character in alphabet
+	
 
-	int rowSize = (sizeof(wordTable) / sizeof(wordTable[1])); ////how many rows (10)
-	int colSize = ((sizeof(wordTable) / rowSize)/sizeof(wordTable[0][0]))-1; 
-	cout << rowSize << '\n';
-	int i = 0;
-	int z = 1;
-	int j;
-	char c;
 
-	exitLoop:
-	while(i < rowSize)
+	//putting words into table
+	for (int i = 0; i < rowSize; i++)
 	{
-		cout << "enter word " << i + 1 << ": ";
-		c = getchar();
-
-		for (; c != '\n';)//puts letters of word into table array
+		for (int j = 0; (c = wordTable[i][j]) && (c != '\n'); j++)
 		{
 			c = toupper(c);
-			wordTable[i][c - 'A'] += 1; //converts letters into decimal equivalent, adds 1 to position in array
-			c = getchar();
+			occTable[i][c - 'A'] += 1;
 		}
-		for (j = i; j > 0; j--) //j = current position (j-- goes up 1 row)
+	}
+
+
+	//counting occurrences 
+	groupTable[0]++;
+	for (int i = 0; i < rowSize; i++)
+	{
+		for (int x = i+1; x < rowSize; x++)
 		{
-			for (int x = 0; x < colSize; x++)
+			while ((occTable[i][j] == occTable[x][j]) && (j < alphaLength))
 			{
-				if (wordTable[i][x] != wordTable[j][x])
-				{
-					z++;
-					wordTable[i][26] = z;
-					cout << wordTable[i][26] << '\n';
-					i++;
-					goto exitLoop;
-				}
+				j++;
+			}
+			if (j == 26) //if all of row 'i' equals all of row 'x', 
+			{
+				groupTable[i]++;
+			}
+			j = 0;
+		}
+	}
+
+
+	//printing
+	for (int i = 0; i < rowSize; i++)
+	{
+		for (int j = 0; j < alphaLength; j++)
+		{
+			if (wordTable[i][j] != ' ' && groupTable[i] != 0)
+			{
+				std::cout << wordTable[i][j];
 			}
 		}
-		wordTable[i][26] = wordTable[j][26];
-		cout << wordTable[i][26] << '\n';
-		i++;
-
+		if (groupTable[i] != 0)
+		{
+			std::cout << " occurs " << groupTable[i] << " times" << std::endl;
+		}
 	}
-	//cout << wordTable;
-
-
-
 }
+		
+
+
 
