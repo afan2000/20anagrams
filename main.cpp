5bd57139d0050fc0aaf6cc1274a8692f67654314
afan2000 <apronfag@gmail.com>
2017-02-19 17:45:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,62 @@
 #include <iostream>
 #include "stdafx.h"
-
+ 
 int main()
 {
-	char wordTable[7][30] = { "abcde", "aba", "abccc", "cccba", "edcba", "cccba", "aba" };
-	int occTable[7][26] = { 0 }; //occurence table that converts wordTable to decimal
-	int groupTable[7] = { 0 };
-	int rowSize = (sizeof(wordTable) / sizeof(wordTable[1])); ////how many rows
+	const int wordLength = 30;
+	char wordTable[][wordLength] = { "abc", "abcd", "abc", "abcde", "abc", "abcd", "abc", "abcde", "abcd", "abcdef", 
+									  "abc", "abcd", "abc", "abcde", "abc", "abcd", "abc", "abcde", "abcd", "abcdef"};
+	int occTable[20][26] = { 0 }; //occurence table that converts wordTable to decimal
+	int groupTable[20] = { 0 };
+	int rowSize = (sizeof(wordTable) / sizeof(wordTable[0])); ////how many rows
 	const int alphaLength = 26; //amount of letters in alphabet
 	char c; //used to get characters
-	int j = 0; //marks position of character in alphabet
-	
+	int j = 0, counter = 0, x, m = 0; //marks position of character in alphabet
+
 
 
 	//putting words into table
 	for (int i = 0; i < rowSize; i++)
 	{
+		//std::cout << "enter word " << i + 1 << ": ";
 		for (int j = 0; (c = wordTable[i][j]) && (c != '\n'); j++)
 		{
 			c = toupper(c);
-			occTable[i][c - 'A'] += 1;
+			wordTable[i][j] = c;
 		}
 	}
+	std::cout << "word table complete" << std::endl;
 
-
-	//counting occurrences 
-	groupTable[0]++;
+	//counting occurrences
+	
 	for (int i = 0; i < rowSize; i++)
-	{
-		for (int x = i+1; x < rowSize; x++)
+	{		
+		for (int p = 0; p < wordLength; p++)
 		{
-			while ((occTable[i][j] == occTable[x][j]) && (j < alphaLength))
+			m = wordTable[i][p] - 'A';
+			if(m >= 0)
+				occTable[i][m]++;
+		}
+		for (x = 0; x < i; x++)
+		{
+			j = 0;
+			while (occTable[i][j] == occTable[x][j] && (j < alphaLength))
 			{
 				j++;
 			}
 			if (j == 26) //if all of row 'i' equals all of row 'x', 
 			{
-				groupTable[i]++;
+				groupTable[i] = groupTable[x];				
+				break;
 			}
-			j = 0;
+		}
+		if (x == i)
+		{
+			counter++;
+			groupTable[i] = counter;
 		}
 	}
+	
 
 
 	//printing
@@ -48,18 +64,14 @@ int main()
 	{
 		for (int j = 0; j < alphaLength; j++)
 		{
-			if (wordTable[i][j] != ' ' && groupTable[i] != 0)
+			if (wordTable[i][j] != ' ')
 			{
 				std::cout << wordTable[i][j];
 			}
+			
 		}
-		if (groupTable[i] != 0)
-		{
-			std::cout << " occurs " << groupTable[i] << " times" << std::endl;
-		}
+		std::cout << " Group: " << groupTable[i] << std::endl;
 	}
 }
-		
-
 
 
